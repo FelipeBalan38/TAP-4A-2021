688087a86d5ed5bf99de885a9f2b5733cea03af8
Felipe Balan Martín <80130461+FelipeBalan38@users.noreply.github.com>
2021-06-22 12:27:53 -0500

    Add files via upload

---
+++ Proyecto2P/ParteMovil/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App7.Servicios;
+using App7.Modelo;
+
+
+namespace App7
+{
+    public partial class MainPage : ContentPage
+    {
+        APIServicio db;
+        public MainPage()
+        {
+            InitializeComponent();
+            db = new APIServicio();
+            ListaPuntos();
+        }
+        private async void ListaPuntos()
+        {
+
+            var response = await APIServicio.ServiceClientInstance.GetPuntos();
+
+            Puntaj.ItemsSource = response;
+
+        }
+
+        void Puntaj_sel(Object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+            var content = e.SelectedItem as Puntuaciones;
+            Puntaj.SelectedItem = null;
+
+        }
+    }
+}
---
+++ Proyecto2P/ParteMovil/Modelo/Modelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App7.Modelo
+{
+    class Puntuaciones
+    {
+        public string nombre { get; set; }
+        public int puntos { get; set; }
+
+    }
+}
