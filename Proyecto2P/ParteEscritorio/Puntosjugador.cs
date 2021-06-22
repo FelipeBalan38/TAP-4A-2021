1d68ebbdbb9cae39eb0d434bb2217b67f4675d7d
Felipe Balan Martín <80130461+FelipeBalan38@users.noreply.github.com>
2021-06-22 12:35:03 -0500

    Add files via upload

---
+++ Proyecto2P/ParteEscritorio/Puntosjugador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiccionarioMovil.Model
+{
+    class Puntosjugador
+    {
+
+        public string Palabra { get; set; }
+        public string Significado { get; set; }
+
+    }
+}
---
+++ Proyecto2P/ParteEscritorio/Servicio/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto.Modelo;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+
+namespace Proyecto.Servicio
+{
+    class ApiServices
+    {
+
+        private static ApiServices _ServiceClientInstance;
+
+        public static ApiServices ServiceClientInstance
+        {
+            get
+            {
+                if (_ServiceClientInstance == null)
+                    _ServiceClientInstance = new ApiServices();
+                return _ServiceClientInstance;
+            }
+        }
+
+        FirebaseClient firebase;
+        public ApiServices()
+        {
+            firebase = new FirebaseClient("https://juego-sanic-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task<bool> registerPuntuacion(string nombre, int puntos)
+        {
+            var result = await firebase
+                .Child("Puntuaciones")
+                .PostAsync(new Puntuaciones()
+                {
+                    nombre = nombre,
+                    puntos = puntos
+                });
+
+            if (result.Object != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+
+
