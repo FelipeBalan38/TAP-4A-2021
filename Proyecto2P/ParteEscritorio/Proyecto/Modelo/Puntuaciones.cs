8feaf2d2f19f1c50bc7c6ba786852a2898e8b7a6
Felipe Balan Martín <80130461+FelipeBalan38@users.noreply.github.com>
2021-06-22 12:31:34 -0500

    Add files via upload

---
+++ Proyecto2P/ParteEscritorio/Proyecto/Modelo/Puntuaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Modelo
+{
+    class Puntuaciones
+    {
+        public string nombre { get; set; }
+        public int puntos { get; set; }
+    }
+}
+
+
+
+
+  
